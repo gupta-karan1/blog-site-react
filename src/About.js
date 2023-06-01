2b742edf72366f0d8a7ac6f0d3ab2f62b7263013
Karan Gupta <d22124440@mytudublin.ie>
2023-06-01 23:08:48 +0100

    about, footer and missing pages done

--- src/About.js
+++ src/About.js
@@ -1,7 +1,11 @@
 const About = () => {
   return (
-    <main>
-      <h1>About</h1>
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is a project to learn and master the fundamentals of the
+        React Framework.
+      </p>
     </main>
   );
 };
