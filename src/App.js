a10bc5328e4fa424e705ae0bfa12946034f88b7f
Karan Gupta <d22124440@mytudublin.ie>
2023-06-02 19:13:43 +0100

    update axios done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Layout from "./Layout";
 import { format } from "date-fns";
 import api from "./api/posts";
+import EditPost from "./Edit.js";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,6 +20,9 @@ function App() {
 
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
 
   const navigate = useNavigate();
 
@@ -81,6 +85,22 @@ function App() {
     }
   };
 
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
   return (
     <Routes>
       <Route
@@ -108,6 +128,20 @@ function App() {
             element={<PostPage posts={posts} handleDelete={handleDelete} />}
           />
         </Route>
+
+        <Route
+          path="/edit/:id"
+          element={
+            <EditPost
+              posts={posts}
+              handleEdit={handleEdit}
+              editBody={editBody}
+              editTitle={editTitle}
+              setEditBody={setEditBody}
+              setEditTitle={setEditTitle}
+            />
+          }
+        />
 
         <Route path="about" element={<About />} />
 
